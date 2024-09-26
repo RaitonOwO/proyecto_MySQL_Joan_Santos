ee33c87c678c5811bf69e2923b46e16b0ccdcae2
RaitonOwO <jhoanowo@gmail.com>
2024-09-25 23:30:41 -0500

    feat: :sparkles: se agregaron los datos a la tabla de regiones

--- dml.sql
+++ dml.sql
@@ -6,3 +6,10 @@ INSERT INTO `mydb`.`roles` (`id_rol`, `nombre`) VALUES
 (3, 'Mid'),
 (4, 'ADC'),
 (5, 'Support');
+
+INSERT INTO `mydb`.`regiones` (`id_region`, `nombre`) VALUES
+(1, 'NA'),
+(2, 'EUW'),
+(3, 'KR'),
+(4, 'LAN'),
+(5, 'LAS');
